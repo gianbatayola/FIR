9368153177af58f70aa45299b5f4be037450df60
Gian Batayola <55165328+gianbatayola@users.noreply.github.com>
2021-12-06 16:18:43 -0500

    Update FIR-diabetes_PCA.py

--- FIR-diabetes_PCA.py
+++ FIR-diabetes_PCA.py
@@ -5,8 +5,10 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+import warnings
+warnings.filterwarnings("ignore")
 
-df = pd.read_csv('dataset_diabetes/diabetic_data.csv')
+df = pd.read_csv('diabetic_data.csv')
 df=df.replace('?',np.nan).dropna(axis = 0, how = 'any') # for sake of ease just remove anything with a ? 
 
 # turn our target variable into a binary
@@ -36,27 +38,46 @@ most_important_names = [initial_feature_names[most_important[i]] for i in range(
 
 dic = {'PC{}'.format(i): most_important_names[i] for i in range(n_pcs)}
 
-df = pd.DataFrame(dic.items())
+df2 = pd.DataFrame(dic.items())
 
-print(df)
-
-plt.plot(np.cumsum(model.explained_variance_ratio_))
-plt.xlabel('number of components')
-plt.ylabel('cumulative explained variance')
-plt.show()
-
+print(df2)
 
 scale = StandardScaler()
 X = StandardScaler().fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3, random_state=0)
-n_test_samples = len(X_test)
 
+print(df.columns)
+
+# top 3 features
+X_train_3 = X_train[:,[0, 1, 6]] 
+X_test_3 = X_test[:, [0, 1, 6]]
+clf = MLPClassifier((10, 10), random_state=1)
+clf.fit(X_train_3, y_train)
+print(clf.score(X_test_3, y_test))
+
+# top 6 features
+X_train_6 = X_train[:,[0, 1, 3, 4, 6, 8]] 
+X_test_6 = X_test[:,[0, 1, 3, 4, 6, 8]] 
+clf = MLPClassifier((10, 10), random_state=1)
+clf.fit(X_train_6, y_train)
+print(clf.score(X_test_6, y_test))
+
+# top 9 features
+X_train_9 = X_train[:,[0, 1, 3, 4, 5, 6, 7, 8, 9]] 
+X_test_9 = X_test[:, [0, 1, 3, 4, 5, 6 ,7 ,8 ,9]]
+clf = MLPClassifier((10, 10), random_state=1)
+clf.fit(X_train_9, y_train)
+print(clf.score(X_test_9, y_test))
+
+# top 12 features
+#X_train_12 = X_train[:,[0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11]] 
+#X_test_12 = X_test[:, [0, 1, 3, 4, 5, 6 ,7 ,8 ,9, 10, 11]]
+#clf = MLPClassifier((10, 10), random_state=1)
+#clf.fit(X_train_12, y_train)
+#print(clf.score(X_test_12, y_test))
+
+# with all features
 clf = MLPClassifier((10, 10), random_state=1)
 clf.fit(X_train, y_train)
-print(clf.score(X_test, y_test)) #we threw out everything important lolol
-
-
-
-
-
+print(clf.score(X_test, y_test))
 
